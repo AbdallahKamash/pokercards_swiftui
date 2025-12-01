2d6dbff64e83b22b46b57e5b8f455499fc4e92b4
AbdallahKamash <abdallah-kamash@outlook.com>
2025-12-01 08:36:20 +0200

    Initial Commit

---
+++ pokercards/pokercardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokercardsApp.swift
+//  pokercards
+//
+//  Created by Abdallah Kamash on 1/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct pokercardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pokercardsTests/pokercardsTests.swift
@@ -0,0 +1,16 @@
+//
+//  pokercardsTests.swift
+//  pokercardsTests
+//
+//  Created by Abdallah Kamash on 1/12/2025.
+//
+
+import Testing
+
+struct pokercardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
